bf783053db8068e428096a95483cb60d2f626014
王盛魁 <wangshengkui@wangshengkuideMacBook-Pro.local>
2017-08-10 22:04:09 +0800

    changeFiles

--- PushTestApp/AppDelegate.m
+++ PushTestApp/AppDelegate.m
@@ -8,7 +8,7 @@
 
 #import "AppDelegate.h"
 
-@interface AppDelegate ()
+@interface AppDelegate ()<UIAlertViewDelegate>
 
 @end
 
@@ -17,9 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [self registerAPN];
     return YES;
 }
-
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -30,14 +30,34 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    NSInteger times = [[[NSUserDefaults standardUserDefaults] objectForKey:@"GoBackground"] integerValue];
+    times++;
+    [[NSUserDefaults standardUserDefaults]setInteger:times forKey:@"GoBackground"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    NSInteger times = [[[NSUserDefaults standardUserDefaults] objectForKey:@"GoBackground"] integerValue];
+    if (times >= 3) {
+        // ios9之后 将会被UIAlertController替代
+        UIAlertView * alert = [[UIAlertView alloc]initWithTitle:@"温馨提示" message:@"打开推送权限" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"允许", nil];
+        [alert show];
+    }
+    
 }
-
-
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    if (buttonIndex != 0) {
+        // 用户点击允许 ios8之后，可以直接调转手机设置界面
+        if([UIDevice currentDevice].systemVersion.integerValue >= 8.0){
+            [self goToSetting];
+        }
+    }
+}
+- (void)goToSetting{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
+}
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
@@ -46,6 +66,39 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
-
-
+- (void)registerAPN {
+    // 在此根据版本不同，设置不同的注册方式 以ios8为分界线
+    if([UIDevice currentDevice].systemVersion.integerValue >= 8.0){
+        UIUserNotificationSettings *setting = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert categories:nil];
+        [[UIApplication sharedApplication] registerUserNotificationSettings:setting];
+    }else{
+        // ios7以下
+        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert ];
+    }
+    
+}
+// 用户允许通知，会进入此回调
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings{
+    // 注册通知
+    [application registerForRemoteNotifications];
+}
+// 注册成功的回调
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken{
+    // 在此回调中获得deviceToken
+    NSString *tokenString = [NSString stringWithFormat:@"%@",deviceToken];
+    // 对数据进行处理，删除<、>、空格
+    tokenString = [tokenString stringByReplacingOccurrencesOfString:@" " withString:@""];
+    tokenString = [tokenString stringByReplacingOccurrencesOfString:@"<" withString:@""];
+    tokenString = [tokenString stringByReplacingOccurrencesOfString:@">" withString:@""];
+    // 将处理好的tokenString上传给服务器
+}
+// 注册失败的回调
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error{
+    
+}
+#pragma mark - LocalPush
+// 点击本地推送通知 进入APP后的回调
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification{
+    [application cancelLocalNotification:notification];
+}
 @end
--- PushTestApp/ViewController.m
+++ PushTestApp/ViewController.m
@@ -16,9 +16,56 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor grayColor];
+    [self creatLocalPush];
 }
-
+// 创建本地推送
+- (void)creatLocalPush{
+    UILocalNotification *localNotifi = [[UILocalNotification alloc]init];
+    // 设置触发时间
+    localNotifi.fireDate = [NSDate dateWithTimeIntervalSinceNow:3];
+    // 设置时区  以当前手机运行的时区为准
+    localNotifi.timeZone = [NSTimeZone defaultTimeZone];
+    // 设置推送 显示的内容
+    localNotifi.alertTitle = @"推送标题";
+    localNotifi.alertBody = @"本地推送内容";
+    localNotifi.alertAction = @"alert提示框按钮文本";
+    //是否显示额外的按钮，为no时alertAction消失
+    localNotifi.hasAction = YES;
+    // 设置 icon小红点个数
+    localNotifi.applicationIconBadgeNumber = 1;
+    // 设置是否重复  重复最小时间间隔为秒，但最好是分钟
+    // 不设置此属性，则默认不重复
+    localNotifi.repeatInterval =  NSCalendarUnitMinute;
+    // 设置推送的声音
+    // 通知的声音默认是iOS系统自带的声音，当然也支持自定义，但是自定义必须是固定格式的音频文件，不支持mp3，最常用的是cef/pcm/ma4/alaw格式的音频文件，并且播放时间不能长于30秒，如果长于30秒，将会被系统时间所代替，音频文件必须放在工程文件当中([NSBundle mainBundle])
+    localNotifi.soundName = UILocalNotificationDefaultSoundName;
+    
+    // 设置推送的区别符
+    localNotifi.userInfo = @{@"name":@"loaclPushOne"};
+    // 按照前面设置的计划 执行此通知
+    [[UIApplication sharedApplication] scheduleLocalNotification:localNotifi];
+}
+// 关闭本地推送
+- (void)deleteLocalPush{
+    // 获取该app上所有的本地推送
+    NSArray *allLocalNotifi = [[UIApplication sharedApplication] scheduledLocalNotifications];
+    
+    // 取消特定的本地推送
+    UILocalNotification *localNotifi  = nil;
+    for (UILocalNotification *item in allLocalNotifi) {
+        NSDictionary *userInfo = item.userInfo;
+        if ([[userInfo objectForKey:@"name"] isEqualToString:@"loaclPushOne"]) {
+            localNotifi = item;
+            break;
+        }
+    }
+    [[UIApplication sharedApplication] cancelLocalNotification:localNotifi];
+    
+    // 取消所有的本地推送
+    [[UIApplication sharedApplication]  cancelAllLocalNotifications];
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
